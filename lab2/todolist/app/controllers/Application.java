b996ec3f1c9dabdd1879cdc4034adca877b85ff0
raiffanderson <raiff.mata@ccc.ufcg.edu.br>
2013-11-28 21:32:24 -0300

    inserindo lab2

---
+++ lab2/todolist/app/controllers/Application.java
@@ -0,0 +1,40 @@
+package controllers;
+
+import com.sun.jmx.snmp.tasks.*;
+
+import play.*;
+import play.mvc.*;
+import play.data.*;
+import models.*;
+import models.Task;
+
+import views.html.*;
+import views.html.helper.form;
+
+public class Application extends Controller {
+	
+	static Form<Task> taskForm = Form.form(Task.class);
+
+    public static Result index() {
+        return redirect(routes.Application.tasks());
+    }
+
+    public static Result tasks(){
+    	return ok(views.html.index.render(Task.all(), taskForm));
+    }
+    
+    public static Result newTask(){
+    	Form<Task> filledForm = taskForm.bindFromRequest();
+    	if (filledForm.hasErrors()){
+    		return badRequest(views.html.index.render(Task.all(),filledForm));
+    	}
+    	else{
+    		Task.create(filledForm.get());
+    		return redirect(routes.Application.tasks());
+    	}
+    }
+    
+    public static Result deleteTask(Long id){
+    	return TODO;
+    }
+}
